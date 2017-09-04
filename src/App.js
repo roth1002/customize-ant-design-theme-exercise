5ecf03cd592f229f29da20d37546bae1534efa95
RothGo <roth1002@gmail.com>
2017-09-04 10:34:58 +0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
-import { Icon, Rate, Tabs } from 'antd';
-const TabPane = Tabs.TabPane;
+import { Rate } from 'antd';
 
 class App extends Component {
   render() {
@@ -10,22 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <Tabs defaultActiveKey="1" onChange={key => console.log(key)}>
-          <TabPane tab="Tab 1" key="1">
-            <h2>Content of Tab Pane 1</h2>
-            <Rate allowHalf defaultValue={2.5}/>
-          </TabPane>
-          <TabPane tab="Tab 2" key="2">
-            <h2>Content of Tab Pane 2</h2>
-            <Rate character={<Icon type="heart" />}/>
-          </TabPane>
-          <TabPane tab="Tab 3" key="3">
-            <h2>Content of Tab Pane 3</h2>
-            <Rate disabled defaultValue={2}/>
-          </TabPane>
-        </Tabs>
       </div>
     );
   }
